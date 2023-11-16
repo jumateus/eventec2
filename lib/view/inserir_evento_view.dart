fdafdfe2e9f2cee28139db245d1bad68d3ec2ff5
jumateus <150052435+jumateus@users.noreply.github.com>
2023-11-16 19:49:00 -0300

    xxx

--- lib/view/inserir_evento_view.dart
+++ lib/view/inserir_evento_view.dart
@@ -154,7 +154,7 @@ void salvarEvento(context,
       EventoController().adicionar(context, e);
       sucesso(context, 'Evento adicionado com sucesso');
     } else {
-       EventoController().atualizar(context, docId, e);
+       EventoController().atualizar(context, docId, e as String);
       _mostrarSnackBar(context, 'Evento atualizado com sucesso');
     }
 
@@ -162,4 +162,8 @@ void salvarEvento(context,
   } catch (e) {
     erro(context, 'Erro ao salvar o evento: $e');
   }
+}
+
+class _mostrarSnackBar {
+  _mostrarSnackBar(context, String s);
 }
